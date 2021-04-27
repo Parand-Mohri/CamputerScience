a03f25af1f1f706609bcc80fbf1a8885ec66268d
Parand-Mohri <72268369+Parand-Mohri@users.noreply.github.com>
2021-04-27 17:00:52 +0200

    delete

--- Unique.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Unique{
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter tree numbers:");
-        int n1 =in.nextInt(), n2 = in.nextInt(), n3 = in.nextInt();
-
-    }
-}
